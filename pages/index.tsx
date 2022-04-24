f9afc941c0461265d2b30b05a8980b1ffad83f4f
li-kathleen <wyliae@connect.ust.hk>
2022-04-24 22:40:21 +0800

    Update frontpage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,131 @@
-import * as React from 'react';
-import type { NextPage } from 'next';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Link from '../src/Link';
-import ProTip from '../src/ProTip';
-import Copyright from '../src/Copyright';
+import * as React from "react";
+import type { NextPage } from "next";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import {
+  Box,
+  Button,
+  Paper,
+  Skeleton,
+  TextField,
+  Typography,
+} from "@mui/material";
+import Image from "next/image";
 
-const Home: NextPage = () => {
+const Pokemon: NextPage = () => {
+  const [id, setId] = useState("1");
+  const [tempId, setTempId] = useState("1");
+  const [spriteFront, setSpriteFront] = useState("");
+  const [spriteBack, setSpriteBack] = useState("");
+  const [spriteName, setSpriteName] = useState("");
+  const [spriteType1, setSpriteType1] = useState("");
+  const [spriteType2, setSpriteType2] = useState<string | null>(null);
+
+  useEffect(() => {
+    setSpriteFront("");
+    setSpriteBack("");
+    axios.get("https://pokeapi.co/api/v2/pokemon/" + id).then(({ data }) => {
+      setSpriteFront(data.sprites.front_default);
+      setSpriteBack(data.sprites.back_default);
+      setSpriteName(data.species.name);
+      setSpriteType1(data.types[0].type.name);
+      if (data.types[1]) {
+        setSpriteType2(data.types[1].type.name);
+      } else {
+        setSpriteType2(null);
+      }
+    });
+  }, [id]);
+
   return (
-    <Container maxWidth="lg">
-      <Box
-        sx={{
-          my: 4,
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Typography variant="h4" component="h1" gutterBottom>
-          MUI v5 + Next.js with TypeScript example
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100vw",
+        height: "100vh",
+      }}
+    >
+      <Paper elevation={4} sx={{ width: "80vw", height: "80vh" }}>
+        <Box mt={2}>
+          <Typography variant="h3" textAlign="center">
+            Find Your Pokemon
+          </Typography>
+        </Box>
+        <Box sx={{ display: "flex", justifyContent: "center" }}>
+          {spriteFront ? (
+            <Image
+              src={spriteFront}
+              alt="pokemon sprite front"
+              width={150}
+              height={150}
+            />
+          ) : (
+            <Skeleton variant="rectangular" width={150} height={150} />
+          )}
+          {spriteBack ? (
+            <Image
+              src={spriteBack}
+              alt="pokemon sprite back"
+              width={150}
+              height={150}
+            />
+          ) : (
+            <Skeleton variant="rectangular" width={150} height={150} />
+          )}
+        </Box>
+        <Typography variant="h4" sx={{ textAlign: "center" }}>
+          {spriteName}
         </Typography>
-        <Link href="/about" color="secondary">
-          Go to the about page
-        </Link>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
+        <Box display="flex" justifyContent="center">
+          <Typography component="span" mx={1}>
+            {spriteType1}
+          </Typography>
+          {spriteType2 && (
+            <Typography component="span" mx={1}>
+              {spriteType2}
+            </Typography>
+          )}
+        </Box>
+        <Box my={2} sx={{ display: "flex", justifyContent: "center" }}>
+          <TextField
+            label="id"
+            type="number"
+            value={tempId}
+            onChange={(e) => {
+              if (e.target.value.includes(".")) return;
+              const value = parseInt(e.target.value);
+              if (value >= 1 && value <= 151) {
+                setTempId(e.target.value);
+              }
+            }}
+          />
+          <Button
+            variant="outlined"
+            sx={{ marginLeft: 1 }}
+            onClick={() => {
+              setId(tempId);
+            }}
+          >
+            Go
+          </Button>
+        </Box>
+        <Box sx={{ display: "flex", justifyContent: "center" }}>
+          <Button
+            variant="contained"
+            onClick={() => {
+              const num = `${Math.floor(Math.random() * 151) + 1}`;
+              setId(num);
+              setTempId(num);
+            }}
+          >
+            Random
+          </Button>
+        </Box>
+      </Paper>
+    </Box>
   );
 };
 
-export default Home;
+export default Pokemon;
