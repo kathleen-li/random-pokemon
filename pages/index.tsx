08501ef00b6fb72a6cb4d9f6915281253b137848
li-kathleen <wyliae@connect.ust.hk>
2022-04-24 22:49:50 +0800

    Add page title

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,6 +12,7 @@ import {
 } from "@mui/material";
 import Image from "next/image";
 import { PokemonImage } from "../src/PokemonImage";
+import Head from "next/head";
 
 const Pokemon: NextPage = () => {
   const [id, setId] = useState("1");
@@ -39,75 +40,80 @@ const Pokemon: NextPage = () => {
   }, [id]);
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        width: "100vw",
-        height: "100vh",
-      }}
-    >
-      <Paper elevation={4} sx={{ width: "80vw", height: "80vh" }}>
-        <Box mt={2}>
-          <Typography variant="h3" textAlign="center">
-            Find Your Pokemon
-          </Typography>
-        </Box>
-        <Box sx={{ display: "flex", justifyContent: "center" }}>
-          <PokemonImage src={spriteFront} alt="pokemon sprite front" />
-          <PokemonImage src={spriteBack} alt="pokemon sprite back" />
-        </Box>
-        <Typography variant="h4" sx={{ textAlign: "center" }}>
-          {spriteName}
-        </Typography>
-        <Box display="flex" justifyContent="center">
-          <Typography component="span" mx={1}>
-            {spriteType1}
-          </Typography>
-          {spriteType2 && (
-            <Typography component="span" mx={1}>
-              {spriteType2}
+    <>
+      <Head>
+        <title>Find Your Pokemon</title>
+      </Head>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100vw",
+          height: "100vh",
+        }}
+      >
+        <Paper elevation={4} sx={{ width: "80vw", height: "80vh" }}>
+          <Box mt={2}>
+            <Typography variant="h3" textAlign="center">
+              Find Your Pokemon
             </Typography>
-          )}
-        </Box>
-        <Box my={2} sx={{ display: "flex", justifyContent: "center" }}>
-          <TextField
-            label="id"
-            type="number"
-            value={tempId}
-            onChange={(e) => {
-              if (e.target.value.includes(".")) return;
-              const value = parseInt(e.target.value);
-              if (value >= 1 && value <= 151) {
-                setTempId(e.target.value);
-              }
-            }}
-          />
-          <Button
-            variant="outlined"
-            sx={{ marginLeft: 1 }}
-            onClick={() => {
-              setId(tempId);
-            }}
-          >
-            Go
-          </Button>
-        </Box>
-        <Box sx={{ display: "flex", justifyContent: "center" }}>
-          <Button
-            variant="contained"
-            onClick={() => {
-              const num = `${Math.floor(Math.random() * 151) + 1}`;
-              setId(num);
-              setTempId(num);
-            }}
-          >
-            Random
-          </Button>
-        </Box>
-      </Paper>
-    </Box>
+          </Box>
+          <Box sx={{ display: "flex", justifyContent: "center" }}>
+            <PokemonImage src={spriteFront} alt="pokemon sprite front" />
+            <PokemonImage src={spriteBack} alt="pokemon sprite back" />
+          </Box>
+          <Typography variant="h4" sx={{ textAlign: "center" }}>
+            {spriteName}
+          </Typography>
+          <Box display="flex" justifyContent="center">
+            <Typography component="span" mx={1}>
+              {spriteType1}
+            </Typography>
+            {spriteType2 && (
+              <Typography component="span" mx={1}>
+                {spriteType2}
+              </Typography>
+            )}
+          </Box>
+          <Box my={2} sx={{ display: "flex", justifyContent: "center" }}>
+            <TextField
+              label="id"
+              type="number"
+              value={tempId}
+              onChange={(e) => {
+                if (e.target.value.includes(".")) return;
+                const value = parseInt(e.target.value);
+                if (value >= 1 && value <= 151) {
+                  setTempId(e.target.value);
+                }
+              }}
+            />
+            <Button
+              variant="outlined"
+              sx={{ marginLeft: 1 }}
+              onClick={() => {
+                setId(tempId);
+              }}
+            >
+              Go
+            </Button>
+          </Box>
+          <Box sx={{ display: "flex", justifyContent: "center" }}>
+            <Button
+              variant="contained"
+              onClick={() => {
+                const num = `${Math.floor(Math.random() * 151) + 1}`;
+                setId(num);
+                setTempId(num);
+              }}
+            >
+              Random
+            </Button>
+          </Box>
+        </Paper>
+      </Box>
+    </>
   );
 };
 
